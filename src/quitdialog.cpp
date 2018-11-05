c46e6274aa9f9ebf593b43ada7fd64c9e39af593
Christoph Hueffelmann <chr@istoph.de>
2018-11-05 19:56:25 +0100

    QuitDialog nameing and defautl button

--- src/quitdialog.cpp
+++ src/quitdialog.cpp
@@ -3,7 +3,7 @@
 QuitDialog::QuitDialog(Tui::ZWidget *parent, QString filename) : Dialog(parent) {
     setGeometry({20, 2, 40, 8});
     setFocus();
-    setWindowTitle("Exit and Save");
+    setWindowTitle("Save and Exit");
 
     Label *l1 = new Label(this);
     l1->setText("Save: " + filename);
@@ -12,12 +12,13 @@ QuitDialog::QuitDialog(Tui::ZWidget *parent, QString filename) : Dialog(parent) 
     Button *b1 = new Button(this);
     b1->setGeometry({2, 5, 10, 7});
     b1->setText("Exit");
-    b1->setDefault(true);
+    //b1->setDefault(true);
 
     Button *b2 = new Button(this);
     b2->setGeometry({18, 5, 19, 7});
     b2->setText("Save and Exit");
     b2->setDefault(true);
+
     QObject::connect(b1, &Button::clicked, this, &QuitDialog::exitSelected);
     QObject::connect(b2, &Button::clicked, this, &QuitDialog::saveSelected);
 
