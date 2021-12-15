b21ee79de086f99c55f0ca09fec5691bdd6ba747
Christoph Hueffelmann <chr@istoph.de>
2021-12-15 23:43:44 +0100

    Refector: Gotoline.

--- src/gotoline.cpp
+++ src/gotoline.cpp
@@ -14,42 +14,34 @@ GotoLine::GotoLine(Tui::ZWidget *parent) : Dialog(parent) {
     vbox->setSpacing(1);
 
     HBoxLayout *hbox1 = new HBoxLayout();
+    Tui::ZLabel *labelGoto = new Tui::ZLabel(this);
+    labelGoto->setText("Goto line: ");
+    hbox1->addWidget(labelGoto);
 
-    //file_goto_line = new Dialog(this);
-    //file_goto_line->setGeometry({20, 2, 40, 8});
-    //file_goto_line->focus();
-
-    Tui::ZLabel *l1 = new Tui::ZLabel(this);
-    l1->setText("Goto line: ");
-    //l1->setGeometry({1,2,12,1});
-    hbox1->addWidget(l1);
-
-    InputBox *i1 = new InputBox(this);
-    //i1->setGeometry({15,2,3,1});
-    i1->setFocus();
-    hbox1->addWidget(i1);
+    InputBox *inputboxGoto = new InputBox(this);
+    inputboxGoto->setFocus();
+    hbox1->addWidget(inputboxGoto);
 
     vbox->add(hbox1);
     vbox->addStretch();
 
     HBoxLayout *hbox2 = new HBoxLayout();
+    Tui::ZButton *buttonCancel = new Tui::ZButton(this);
+    buttonCancel->setGeometry({3, 5, 7, 7});
+    buttonCancel->setText("Cancel");
+    hbox2->addWidget(buttonCancel);
 
-    Tui::ZButton *b0 = new Tui::ZButton(this);
-    b0->setGeometry({3, 5, 7, 7});
-    b0->setText(" Cancel");
-    hbox2->addWidget(b0);
-
-    Tui::ZButton *b1 = new Tui::ZButton(this);
-    b1->setGeometry({15, 5, 7, 7});
-    b1->setText(" OK");
-    b1->setDefault(true);
-    hbox2->addWidget(b1);
+    Tui::ZButton *buttonOK = new Tui::ZButton(this);
+    buttonOK->setGeometry({15, 5, 7, 7});
+    buttonOK->setText("OK");
+    buttonOK->setDefault(true);
+    hbox2->addWidget(buttonOK);
 
     vbox->add(hbox2);
 
-    QObject::connect(b0, &Tui::ZButton::clicked, this, &GotoLine::rejected);
-    QObject::connect(b1, &Tui::ZButton::clicked, this, [this, i1]{
-        lineSelected(i1->text());
+    QObject::connect(buttonCancel, &Tui::ZButton::clicked, this, &GotoLine::rejected);
+    QObject::connect(buttonOK, &Tui::ZButton::clicked, this, [this, inputboxGoto]{
+        lineSelected(inputboxGoto->text());
         this->deleteLater();
     });
 }
