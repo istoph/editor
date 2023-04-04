75b7b6356ee42e87b557083dd7be432c30bdbe6b
Christoph Hueffelmann <chr@istoph.de>
2023-04-04 22:31:15 +0200

    Tests: Filetests: Add search smiley and multiline tests

--- src/tests/filetests.cpp
+++ src/tests/filetests.cpp
@@ -829,7 +829,98 @@ TEST_CASE("actions") {
         CHECK(f->isSelect() == true);
     }
 
+    SECTION("search-aa") {
+        EventRecorder recorder;
+        auto cursorSignal = recorder.watchSignal(f, RECORDER_SIGNAL(&File::cursorPositionChanged));
 
+        bool backword = GENERATE(true, false);
+        CAPTURE(backword);
+        bool reg = GENERATE(true, false);
+        CAPTURE(reg);
+        f->setRegex(reg);
+
+        f->selectAll();
+        f->insertAtCursorPosition(" aa\naa");
+        CHECK(f->getCursorPosition() == QPoint{2,1});
+        CHECK(f->isSelect() == false);
+        recorder.clearEvents();
+
+        f->setSearchText("aa");
+
+        t.f3(backword, &terminal, f);
+        recorder.waitForEvent(cursorSignal);
+        CHECK(f->getCursorPosition() == QPoint{3,0});
+        recorder.clearEvents();
+        CHECK(f->isSelect() == true);
+
+        t.f3(backword, &terminal, f);
+        recorder.waitForEvent(cursorSignal);
+        CHECK(f->getCursorPosition() == QPoint{2,1});
+        recorder.clearEvents();
+        CHECK(f->isSelect() == true);
+    }
+
+    SECTION("search-smiley") {
+        EventRecorder recorder;
+        auto cursorSignal = recorder.watchSignal(f, RECORDER_SIGNAL(&File::cursorPositionChanged));
+
+        bool backword = GENERATE(true, false);
+        CAPTURE(backword);
+        bool reg = GENERATE(true, false);
+        CAPTURE(reg);
+        f->setRegex(reg);
+
+        f->selectAll();
+        f->insertAtCursorPosition("😎😎");
+        CHECK(f->getCursorPosition() == QPoint{4,0});
+        CHECK(f->isSelect() == false);
+        recorder.clearEvents();
+
+        f->setSearchText("😎");
+
+        t.f3(backword, &terminal, f);
+        recorder.waitForEvent(cursorSignal);
+        CHECK(f->getCursorPosition() == QPoint{2,0});
+        recorder.clearEvents();
+        CHECK(f->isSelect() == true);
+
+        t.f3(backword, &terminal, f);
+        recorder.waitForEvent(cursorSignal);
+        CHECK(f->getCursorPosition() == QPoint{4,0});
+        recorder.clearEvents();
+        CHECK(f->isSelect() == true);
+    }
+
+    SECTION("search-smiley-multiline") {
+        EventRecorder recorder;
+        auto cursorSignal = recorder.watchSignal(f, RECORDER_SIGNAL(&File::cursorPositionChanged));
+
+        bool backword = GENERATE(true, false);
+        CAPTURE(backword);
+        bool reg = GENERATE(true, false);
+        CAPTURE(reg);
+        f->setRegex(reg);
+
+        f->selectAll();
+        f->insertAtCursorPosition("😎\n😎");
+        CHECK(f->getCursorPosition() == QPoint{2,1});
+        CHECK(f->isSelect() == false);
+        recorder.clearEvents();
+
+        f->setSearchText("😎");
+
+        t.f3(backword, &terminal, f);
+        recorder.waitForEvent(cursorSignal);
+        CHECK(f->getCursorPosition() == QPoint{2,0});
+        recorder.clearEvents();
+        CHECK(f->isSelect() == true);
+
+        t.f3(backword, &terminal, f);
+        recorder.waitForEvent(cursorSignal);
+        CHECK(f->getCursorPosition() == QPoint{2,1});
+        recorder.clearEvents();
+        CHECK(f->isSelect() == true);
+    }
 
     SECTION("replace") {
         EventRecorder recorder;
