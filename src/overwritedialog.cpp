e4b37fca0c88f0e1bb6d2d631f2b24c84dddc84d
Martin Hostettler <textshell@uchuujin.de>
2023-12-20 21:47:35 +0100

    OverwriteDialog: Add ZWindow::DeleteOnClose flag.

--- src/overwritedialog.cpp
+++ src/overwritedialog.cpp
@@ -8,7 +8,7 @@
 
 
 OverwriteDialog::OverwriteDialog(Tui::ZWidget *parent, QString fileName) : Tui::ZDialog(parent) {
-    setOptions(Tui::ZWindow::MoveOption | Tui::ZWindow::AutomaticOption);
+    setOptions(Tui::ZWindow::MoveOption | Tui::ZWindow::AutomaticOption | Tui::ZWindow::DeleteOnClose);
     setContentsMargins({1, 1, 2, 1});
     setWindowTitle("Overwrite?");
 
