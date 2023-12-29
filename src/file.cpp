9c3c094ef50528d1d5fef4fd225f4cb92a33e335
Christoph Hueffelmann <chr@istoph.de>
2023-12-29 23:43:55 +0100

    fix file: delete on last line with selection.

--- src/file.cpp
+++ src/file.cpp
@@ -1870,7 +1870,7 @@ void File::keyEvent(Tui::ZKeyEvent *event) {
     } else if (event->key() == Qt::Key_Delete && (event->modifiers() == 0 || event->modifiers() == Qt::ControlModifier)) {
         disableDetachedScrolling();
         setSelectMode(false);
-        if (hasBlockSelection()) {
+        if (hasBlockSelection() || hasSelection()) {
             delAndClearSelection();
         } else if (hasMultiInsert()) {
             if (event->modifiers() & Qt::ControlModifier) {
