8bed03d0ab01b2861bc2fe8fdb22a4eae2c7cd1b
Christoph Hueffelmann <chr@istoph.de>
2025-01-10 21:34:48 +0100

    edit: _cmdFormatting add disable if no window detected.

--- src/edit.cpp
+++ src/edit.cpp
@@ -673,7 +673,7 @@ void Editor::enableFileCommands(bool enable) {
     _cmdSortSelectedLines->setEnabled(enable);
     _cmdTab->setEnabled(enable);
     _cmdLineNumbers->setEnabled(enable);
-    //_cmdFormatting->setEnabled(enable);
+    _cmdFormatting->setEnabled(enable);
     _cmdBrackets->setEnabled(enable);
 #ifdef SYNTAX_HIGHLIGHTING
     _cmdSyntaxHighlight->setEnabled(enable);
