0f39c3bccb82c5914dd19fcb34c8c9692fec347d
Martin Hostettler <textshell@uchuujin.de>
2025-04-06 14:57:36 +0200

    Switch selection length display to use clusters instead of utf16 codeunits.

--- src/file.cpp
+++ src/file.cpp
@@ -2570,9 +2570,10 @@ void File::adjustScrollPosition() {
     scrollRangeChanged(std::max(0, max - viewWidth), std::max(0, document()->lineCount() - geometry().height()));
 
     if (hasSelection()) {
+        // TODO consider running this in background
         auto l = getSelectedLines();
         int lines = 1 + std::max(l.second, l.first) - std::min(l.second, l.first);
-        selectCharLines(selectedText().count(), lines);
+        selectCharLines(terminal()->textMetrics().sizeInClusters(selectedText()), lines);
     } else {
         selectCharLines(0, 0);
     }
